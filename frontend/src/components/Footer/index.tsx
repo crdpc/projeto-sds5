aaf73fe97d78feeb73a81fcefeb99d984be12d48
Carlos Domingues <crdpc@yahoo.com.br>
2021-11-02 12:57:34 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/crdpc" target="_blank" rel="noreferrer">Carlos Domingues</a></p>
+                <p className="text-light"><small><strong>Semana 5 do Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
